4ebe2a05fbb91f2da902a671e22a4f3a7ee3407d
seanodea <seanodea11@gmail.com>
2022-04-02 14:46:46 -0400

    attempting to iterate thru/display emissions data

--- src/index.js
+++ src/index.js
@@ -2,16 +2,39 @@
 // import {testData} from './data.js';
 
 let seaLvl = require('../data/seaLevel.json');
-const maxIceMSM = require('../data/maxArcticIceMSM.json');
+let maxIceMSM = require('../data/maxArcticIceMSM.json');
+let greenhouseGas = require('../data/greenhouseGasEmissions.json');
+let json = require('../data/testmap.json');
 import DataUtils from "./data.js";
 
 
 document.addEventListener("DOMContentLoaded", () => {
-  d3.select("h1").style("background-color", "yellow");
-  let projection = d3.geoEquirectangular();
+  setInterval( () => {
+
+  }, 5)
+  for(let i = 0; i < greenhouseGas[0].length; i++) {
+
+  }
+  console.log(greenhouseGas[0])
+  console.log(json[0].features[0].geometry.coordinates[0][0][0]);
+  console.log(json[0].features[0].geometry.coordinates[0][0][1]);
   
-  console.log(projection([-3.0026, 16.7666]));
+  // console.log(geoJSON);
+
+  // d3.select('.map-container')
+  //   .selectAll('p')
+  //   .data(maxIceMSM)
+  //   .enter()
+  //   .append('div')
+  //   .classed('bar', true);
+    
+
+  // d3.select("h1").style("background-color", "aqua");
+
+  // let projection = d3.geoEquirectangular();
   
+  // console.log(projection([-3.0026, 16.7666]));
+
   // console.log(seaLvl);
   // seaLvl.forEach((el) => {
   //   console.log(el.year);
@@ -21,5 +44,4 @@ document.addEventListener("DOMContentLoaded", () => {
   //  console.log(dataset);
   //  return dataset.seaLvl();
 });
-
 
