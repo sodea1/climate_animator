245a7af9f857bcb993403099727b2ecbdc050294
seanodea <seanodea11@gmail.com>
2022-04-03 12:10:04 -0400

    json revisions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
 import DataUtils, { glaciers, iceCapArea, seaLevel, tonnes } from "./data.js";
-import { seaIceMapData } from "./map.js";
+import { seaIceMapData, testing } from "./map.js";
 
 
 document.addEventListener("DOMContentLoaded", () => {
-  // window.emissionsData = tonnes();
-  // window.iceAreaData = iceCapArea();
-  // window.sealvl = seaLevel();q
-  window.glacier = seaIceMapData();
+//   // window.emissionsData = tonnes();
+//   // window.iceAreaData = iceCapArea();
+//   // window.sealvl = seaLevel();q
+//   // window.glacier = seaIceMapData();
+
   
+//   // let features = topojson.feature(testing);
+//   // console.log(features);
+//   // d3.json("../iceExtent.json", function (error, iceExtent) {
+//   //   if (error) return console.error(error);
+//   //   console.log(iceExtent);
+//   // })
+
+  d3.json("iceExtent.json", function (error, iceExtent) {
+
+    if (error) return console.error(error);
+    console.log(iceExtent);
+  })  
 });
 
--- src/map.js
+++ src/map.js
@@ -1,23 +1,41 @@
-// const iceExtData = require('../data/masie_ice_r00_v01_2022091_4km.json');
-// const topo = require('masie_ice_topo.json');
+export let testing = d3.json("../data/masie_ice_topo.json");
 
 //innerHeight/Width of window return pixel width/height
 // document.DocumentElement returns Element which is root of document aka <html> tag
-let width = Math.max(document.documentElement.clientWidth, window.innerWidth);
-let height = Math.max(document.documentElement.clientHeight, window.innerHeight);
 
-let svg = d3.select("body").append("svg");
+// export let geoJSON = topojson.feature()
+// let width = Math.max(document.documentElement.clientWidth, window.innerWidth);
+// let height = Math.max(document.documentElement.clientHeight, window.innerHeight);
+
+// let svg = d3.select("body").append("svg");
 
 
-// viewBox = define position & dimension(min-x, min-y, width, height)
-// preserveAspectRatio provides scaling to reconcile aspect ratio differences of viewBox and viewPort
-svg.attr("viewBox", "50 50" + width + " " + height)
-    .attr("preserveAspectRatio", "xMinyMin");
+// // viewBox = define position & dimension(min-x, min-y, width, height)
+// // preserveAspectRatio provides scaling to reconcile aspect ratio differences of viewBox and viewPort
+// svg.attr("viewBox", "50 50" + width + " " + height)
+//     .attr("preserveAspectRatio", "xMinyMin");
 
-let map = svg.append("g")
-    .attr("class", "ice-map");
+// let map = svg.append("g")
+//     .attr("class", "ice-map");
 
-// async queue to fetch the json map data, execute renderMap function upon completion
-d3.queue()
-    .defer(d3.json, "../data/masie_ice_topo.json")
+// // async queue to fetch the json map data, execute renderMap function upon completion
+// // first defer === topoJSON passed into await
+// d3.queue()
+//     .defer(d3.json, "../data/masie_ice_topo.json")
+//     .await(function (error, topo) {
+//         if (error) {
+//             console.error(error);
+//         } else {
+//             renderMap(topoJSON);
+//         }
+//     });
+
+// function renderMap(topo) {
+//     let projection = d3.geoMercator()
+//         .scale(120)
+//         .translate();
     
+//     let path = d3.geoPath().projection(projection);
+    
+//     let features = topojson.feature(topo, topo.objects.)
+// }
