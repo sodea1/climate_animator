691f9d8dfc8916da75ae89172dbf13c4d520867b
seanodea <seanodea11@gmail.com>
2022-04-03 18:30:24 -0400

    can't render map

--- src/map.js
+++ src/map.js
@@ -1,3 +1,5 @@
+import { schemeBrBG } from "d3";
+
 export const iceMapData = require("../masie_ice_r00_v01_2022091_4km.json");
 
 export function buildFramework() {
@@ -26,24 +28,50 @@ export function buildFramework() {
 }
 
 export function convertCoords() {
-    let features = iceMapData;
+    // let features = iceMapData['features'];
+    let width = 800;
+    let height = 700;
+    let margin = 50
+    // console.log(features);
+    let features = d3.json("https://raw.githubusercontent.com/sodea1/javascript_project/main/masie_ice_r00_v01_2022091_4km.json")
+    console.log(features);
+    let oneFeat = features[0];
     
-    let svg = d3.select('body')
-        .append('svg')
-        .attr("width", 900)
-        .attr("height", 800);
 
-    let projection = d3.geoMercator();
-
-    let path = d3.geoPath().projection(projection);
-
-    d3.json("../masie_ice_r00_v01_2022091_4km.json", function(err, jsonObj) {
-        console.log(jsonObj);
-    }) 
+    // let projection = d3.geoMercator()
+    //     .fitExtent([margin, margin, [width - margin, height - margin]], features)
     
-    // features.forEach((feat) => {
-    //     console.log(feat.geometry.coordinates);
-    // })
+    
+    
+    // let svg = d3.select("body")
+    //     .append("svg")
+    //     .attr("width", width)
+    //     .attr("height", height);
+
+    // let projection = d3.geoMercator()
+    //     .fitExtent([margin, margin, [width - margin, height - margin]], features)
+    //     // .translate([width, height])
+    //     // .scale(100);
+
+    // let path = d3.geoPath().projection(projection);
+    
+    // // d3.json("../masie_ice_r00_v01_2022091_4km.json")
+    // //     .then(function(data) {
+            
+    // //         svg.append("path")
+    // //             .attr("d", path(data));
+    // //     }
+    // // ) 
+    // // projection.fitSize([width, height], features)
+
+    // svg.selectAll("path")
+    //     .data(features)
+    //     .enter()
+    //     .append('path')
+    //     .attr('d', path)
+    //     .attr('class', 'ice-coords')
+    //     .attr("stroke", 'red')
+    
 }
 
 
