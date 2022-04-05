52a6309faa7a5ef27591a024cacd94a118e37adb
App Academy Student <email@email.com>
2022-04-05 12:25:13 -0400

    start line graph

---
+++ src/emissions_graph.js
@@ -0,0 +1,12 @@
+//tonnes is {year: tonnes}
+const emissionsGraph = () => {
+  let data = 
+  let width = 400;
+  let height = 400;
+
+  let svg = d3.select("#line-graph")
+    .append("svg")
+    .attr("width", width)
+    .attr("height", height)
+    .append("g");
+};
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import { buttonMaker } from "./buttons";
-import { txtGetter } from "./interactions";
+import { tonnes } from "./data";
+// import { txtGetter } from "./interactions";
 import { renderMap, iceMap1980, animate } from "./map";
 
 
@@ -7,15 +8,5 @@ document.addEventListener("DOMContentLoaded", () => {
   renderMap(iceMap1980);
   animate();  
   buttonMaker();
-  txtGetter();
-
-//   const fs = require('fs');
-
-//   fs.readFile('/Users/joe/test.txt', 'utf8', (err, data) => {
-//     if (err) {
-//       console.error(err);
-//       return;
-//     }
-//     console.log(data);
-//   });
+  tonnes()
 });
