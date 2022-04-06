4c298bbffd71a7fc36e698fe4679e0dae50ab5dc
App Academy Student <email@email.com>
2022-04-06 16:39:09 -0400

    added dynamic coloring of map buttons

--- src/buttons.js
+++ src/buttons.js
@@ -4,17 +4,28 @@ import { renderMap, years } from "./map";
 export const buttonMaker = () => {
     let yrs = ["1980", "1990", "2003", "2015"];
     for(let i = 0; i < yrs.length; i++) {
-        document.getElementById(yrs[i]).addEventListener("click", () => {
+        let button = document.getElementById(yrs[i]);
+
+        button.addEventListener("click", () => {
+            button.classList.add('hidden-button');
             let currentMapYear = document.getElementsByClassName("map-year");
             let map;
+
             years.forEach((el) => {
-                if (el[1].toString() === yrs[i]) {
+                if (el[1].toString() === yrs[i]) { // yrs[i] = current year
                     map = el[0];
                 }
-            })
+            });
+
             if (currentMapYear === yrs[i]) {
                 return;
             } else {
+                // remove hidden-button class from all buttons except the one selected
+                for (let j = 0; j < yrs.length; j++) {
+                    if (yrs[i] !== yrs[j]) {
+                        document.getElementById(yrs[j]).classList.remove("hidden-button");
+                    }
+                }
                 document.querySelector("#map-box").remove();
                 renderMap(map);
             }
@@ -22,3 +33,6 @@ export const buttonMaker = () => {
         });
     }
 };
+
+// yrs[i] year clicked (add hidden-button AKA do nothing)
+// 
