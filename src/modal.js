30c1bece5c2554119ab4143bfbb00d658a1c87dd
seanodea <seanodea11@gmail.com>
2022-06-13 17:28:25 -0400

    added escape for modal and click outside to close

--- src/modal.js
+++ src/modal.js
@@ -5,5 +5,18 @@ export const instructions = () => {
     continueBtn[0].addEventListener("click", () => {
         let modal = document.getElementsByClassName("modal");
         modal[0].classList.add("hide");
+    });
+
+    let modalCtn = document.getElementsByClassName("modal");
+    modalCtn[0].addEventListener("click", (e) => {
+        if (e.target.classList[0].includes("modal")) {
+            modalCtn[0].classList.add("hide");
+        }
+    });
+
+    document.addEventListener("keydown", (e) => {
+        if (e.key === "Escape") {
+            modalCtn[0].classList.add("hide");
+        }
     })
 }
